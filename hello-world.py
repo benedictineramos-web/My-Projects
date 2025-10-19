27b47efc3fa511e8c9c797b77c3ed78d5ccafaea
Benedictine Ramos <benedictine.ramos@gmail.com>
2025-10-19 11:59:53 -0400

    Update on my previous script adding my personal message on how I feel about the program

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,4 @@
 print("Hello World!")
 print("Learning about commits today!")
+print("There is so much that I am so excited to learn with Data Science. I know this is just the beginning but")
+print("I know I will be able to get used to coding and programming with time and practice.")
